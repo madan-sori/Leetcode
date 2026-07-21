c5c3d90527fda22f7bf9bb561387fde916171856
Madan Sori <152476531+madan-sori@users.noreply.github.com>
2026-07-21 09:25:22 +0530

    Time: 15 ms (98.26%) | Memory: 45.7 MB (14.03%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+
+        int minDiff = Integer.MAX_VALUE;
+        int result = 0;
+
+        for(int i = 0; i < nums.length - 2; i++) {
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                int diff = Math.abs(sum - target);
+
+                if(diff < minDiff) {
+                    minDiff = diff;
+                    result = sum;
+                }
+
+                if(sum < target) {
+                    left++;
+                }
+                else if(sum > target) {
+                    right--;
+                }
+                else {
+                    return sum;
+                }
+            }
+        }
+
+        return result;
+    }
+}
