5d0eace686ea7c6fa7006698421a082fe85019e3
Madan Sori <152476531+madan-sori@users.noreply.github.com>
2026-07-26 21:49:41 +0530

    Time: 7 ms (55.35%) | Memory: 59.4 MB (47.67%) - LeetSync

---
+++ 335-self-crossing/self-crossing.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+
+        for (int i = 3; i < distance.length; i++) {
+
+            // Case 1
+            if (distance[i] >= distance[i - 2] &&
+                distance[i - 1] <= distance[i - 3])
+                return true;
+
+            // Case 2
+            if (i >= 4 &&
+                distance[i - 1] == distance[i - 3] &&
+                distance[i] + distance[i - 4] >= distance[i - 2])
+                return true;
+
+            // Case 3
+            if (i >= 5 &&
+                distance[i - 2] >= distance[i - 4] &&
+                distance[i] + distance[i - 4] >= distance[i - 2] &&
+                distance[i - 1] <= distance[i - 3] &&
+                distance[i - 1] + distance[i - 5] >= distance[i - 3])
+                return true;
+        }
+
+        return false;
+    }
+}
