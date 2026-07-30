d06b9463474c900dfeefe9c33e1aae2f9751c97d
Madan Sori <152476531+madan-sori@users.noreply.github.com>
2026-07-30 22:18:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (7.51%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,31 +1,13 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
 
-        ArrayList<Integer> X = new ArrayList<>();
-        ArrayList<Integer> y = new ArrayList<>();
         int[] ans = new int[2 * n];
 
-        for(int i = 0; i < nums.length; i++){
-
-            if( i < n){
-
-                X.add(nums[i]);
-            }else{
-
-                y.add(nums[i]);
-
-            }
-
-
-
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
         }
-        for(int i = 0; i < n; i++){
 
-            ans[2*i] = X.get(i);
-            ans[2*i + 1] = y.get(i);
-
-        }
-        
         return ans;
     }
 }
