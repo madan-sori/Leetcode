24453b516f5926ab562b4ea346b752a151a0ae55
Madan Sori <152476531+madan-sori@users.noreply.github.com>
2026-08-20 13:49:24 +0530

    Time: 390 ms (41.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,11 @@
+SELECT
+    
+    e1.name                                 
+    
+FROM Employee e1
+   JOIN Employee e2
+ON e1.id = e2.managerId
+group by 
+ e1.name,
+ e1.id
+having count(e1.id)>=5
