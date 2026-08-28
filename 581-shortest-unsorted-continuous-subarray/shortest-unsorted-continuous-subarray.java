13fd0e1ea21ee56471ce004081276865b9df2545
Madan Sori <152476531+madan-sori@users.noreply.github.com>
2026-08-28 13:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (71.00%) - LeetSync

---
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+
+        int n = nums.length;
+
+        int left = -1;
+        int right = -1;
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        // Find the right boundary
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] < max) {
+                right = i;
+            } else {
+                max = nums[i];
+            }
+        }
+
+        // Find the left boundary
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (nums[i] > min) {
+                left = i;
+            } else {
+                min = nums[i];
+            }
+        }
+
+        if (left == -1) {
+            return 0;
+        }
+
+        return right - left + 1;
+    }
+}
