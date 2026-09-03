3c14ad9ba6de6a1c27d8cb4e3d8a1d92351bf6a8
Madan Sori <152476531+madan-sori@users.noreply.github.com>
2026-09-03 18:44:55 +0530

    Time: 3 ms (95.94%) | Memory: 52.3 MB (35.72%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,53 +1,29 @@
 class Solution {
 
-    public int find(int[] frq) {
-
-        return frq[0];   // number of zeros
-    }
-
     public int longestOnes(int[] nums, int k) {
 
-        int[] frq = new int[2];
-
-        int len = 0;
-        int diff = 0;
-
         int left = 0;
-        int right = 0;
+        int zeros = 0;
+        int maxLen = 0;
 
-        int maxlen = 0;
+        for (int right = 0; right < nums.length; right++) {
 
-        while (right < nums.length) {
-
-            // Add element to window
-            frq[nums[right]]++;
-
-            // Number of zeros in window
-            int maxcount = find(frq);
-
-            len = right - left + 1;
-
-            diff = maxcount;
-
-            // Too many zeros
-            while (diff > k) {
-
-                frq[nums[left]]--;
-
-                left++;
-
-                maxcount = find(frq);
-
-                len = right - left + 1;
-
-                diff = maxcount;
+            if (nums[right] == 0) {
+                zeros++;
             }
 
-            maxlen = Math.max(maxlen, len);
+            while (zeros > k) {
 
-            right++;
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
         }
 
-        return maxlen;
+        return maxLen;
     }
 }
